2b7d7abe034b9c1fba5322822b5e6c84b8841143
pangfumin <pangfumin@gmail.com>
2020-06-13 22:26:56 +0800

    cuda geometry.h

--- geometry.h
+++ geometry.h
@@ -7,9 +7,9 @@
 #include <iostream>
 
 template <size_t DIM, typename T> struct vec {
-    vec() { for (size_t i=DIM; i--; data_[i] = T()); }
-          T& operator[](const size_t i)       { assert(i<DIM); return data_[i]; }
-    const T& operator[](const size_t i) const { assert(i<DIM); return data_[i]; }
+    __host__ __device__ vec() { for (size_t i=DIM; i--; data_[i] = T()); }
+    __host__ __device__      T& operator[](const size_t i)       { assert(i<DIM); return data_[i]; }
+    __host__ __device__ const T& operator[](const size_t i) const { assert(i<DIM); return data_[i]; }
 private:
     T data_[DIM];
 };
@@ -20,44 +20,47 @@ typedef vec<3, int  > Vec3i;
 typedef vec<4, float> Vec4f;
 
 template <typename T> struct vec<2,T> {
-    vec() : x(T()), y(T()) {}
-    vec(T X, T Y) : x(X), y(Y) {}
-    template <class U> vec<2,T>(const vec<2,U> &v);
-          T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
-    const T& operator[](const size_t i) const { assert(i<2); return i<=0 ? x : y; }
+    __host__ __device__ vec() : x(T()), y(T()) {}
+    __host__ __device__ vec(T X, T Y) : x(X), y(Y) {}
+    __host__ __device__       T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
+    __host__ __device__ const T& operator[](const size_t i) const { assert(i<2); return i<=0 ? x : y; }
     T x,y;
 };
 
 template <typename T> struct vec<3,T> {
-    vec() : x(T()), y(T()), z(T()) {}
-    vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
-          T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
-    const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
-    float norm() { return std::sqrt(x*x+y*y+z*z); }
-    vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
+    __host__ __device__ vec() : x(T()), y(T()), z(T()) {}
+    __host__ __device__ vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
+    __host__ __device__       T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+    __host__ __device__ const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+    __host__ __device__ float norm() { return std::sqrt(x*x+y*y+z*z); }
+    __host__ __device__ vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
     T x,y,z;
 };
 
 template <typename T> struct vec<4,T> {
-    vec() : x(T()), y(T()), z(T()), w(T()) {}
-    vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
-          T& operator[](const size_t i)       { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
-    const T& operator[](const size_t i) const { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
+    __host__ __device__ vec() : x(T()), y(T()), z(T()), w(T()) {}
+    __host__ __device__ vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
+    __host__ __device__       T& operator[](const size_t i)       { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
+    __host__ __device__ const T& operator[](const size_t i) const { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
     T x,y,z,w;
 };
 
-template<size_t DIM,typename T> T operator*(const vec<DIM,T>& lhs, const vec<DIM,T>& rhs) {
+
+template<size_t DIM,typename T>
+__host__ __device__ T operator*(const vec<DIM,T>& lhs, const vec<DIM,T>& rhs) {
     T ret = T();
     for (size_t i=DIM; i--; ret+=lhs[i]*rhs[i]);
     return ret;
 }
 
-template<size_t DIM,typename T>vec<DIM,T> operator+(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
+template<size_t DIM,typename T>
+__host__ __device__ vec<DIM,T> operator+(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
     for (size_t i=DIM; i--; lhs[i]+=rhs[i]);
     return lhs;
 }
 
-template<size_t DIM,typename T>vec<DIM,T> operator-(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
+template<size_t DIM,typename T>
+__host__ __device__ vec<DIM,T> operator-(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
     for (size_t i=DIM; i--; lhs[i]-=rhs[i]);
     return lhs;
 }
@@ -68,11 +71,13 @@ template<size_t DIM,typename T,typename U> vec<DIM,T> operator*(const vec<DIM,T>
     return ret;
 }
 
-template<size_t DIM,typename T> vec<DIM,T> operator-(const vec<DIM,T> &lhs) {
+template<size_t DIM,typename T>
+__host__ __device__ vec<DIM,T> operator-(const vec<DIM,T> &lhs) {
     return lhs*T(-1);
 }
 
-template <typename T> vec<3,T> cross(vec<3,T> v1, vec<3,T> v2) {
+template <typename T>
+__host__ __device__ vec<3,T> cross(vec<3,T> v1, vec<3,T> v2) {
     return vec<3,T>(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
 }
 
